66a930cbbed912283b0d7525466819f836ce8d0b
Orien Vandenbergh <orien@icebergh.us>
2016-05-20 03:17:36 -0400

    Updated python objective aggregator, can probably retire

--- get_objectives.py
+++ get_objectives.py
@@ -2,10 +2,18 @@
 
 import requests
 import time
+import json
 
-objectives = requests.get('https://api.guildwars2.com/v2/wvw/objectives')
+#objectives = requests.get('https://api.guildwars2.com/v2/wvw/objectives')
+#
+#for id in objectives.json():
+#  objective = requests.get('http://api.guildwars2.com/v2/wvw/objectives/%s' % id)
+#  print objective.text
+#  time.sleep(2)
 
-for id in objectives.json():
-  objective = requests.get('http://api.guildwars2.com/v2/wvw/objectives/%s' % id)
-  print objective.text
-  time.sleep(2)
+objective_ids = requests.get('https://api.guildwars2.com/v2/wvw/objectives').json()
+
+objective_list = requests.get('https://api.guildwars2.com/v2/wvw/objectives',data = { 'ids': ','.join(objective_ids)}).json()
+
+#print json.dumps(objective_list, indent=4, separators=(',', ': '))
+print json.dumps(objective_list)
