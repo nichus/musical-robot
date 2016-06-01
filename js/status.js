f3adaf07bd00ced310d9365fc5b3a7d13226a66c
Orien Vandenbergh <orien@icebergh.us>
2016-06-01 12:26:19 -0600

    Merged MatchStaus and Status objects into the status.js file

--- js/status.js
+++ js/status.js
@@ -1,13 +1,40 @@
-class Status {
-  constructor(id) {
-    this.ready = new Promise.reject();
+class MatchStatus {
+  constructor() {
+    this._		= {};
+    this._.lastUpdate 	= 0;
+    this._.tick		= 500;
+    this._.interval	= 5000;
+    this._.wid;
+    this._.matchActive	= false;
+    this._.baseURL	= 'https://api.guildwars2.com/v2/wvw/matches?world_id=';
   }
-  fetch() {
-    function
-    this.ready = new Promise(function(fulfill,reject) {
-      fetch("https://api.guildwars2.com/v2/wvw/matches?world=" + this.world_id)
+  start(wid) {
+    if (this._.wid != wid) {
+      this._.wid = wid;
+    }
+    this._.matchActive	= true;
+    this._.lastUpdate	= 0;
+  }
+  stop() {
+    this._.matchActive	= false;
+  }
+  get url {
+    if (this._.wid != undefined) {
+      return this._.baseURL + this._.wid;
+    } else {
+      return undefined;
+    }
+  }
+  update() {
+    if (this._.matchActive && (this.url != undefined)) {
+      fetch(this.url)
 	.then(status)
-	.then(function(request){ return request.json(); });
-    });
+	.then(function(response) { return response.json() })
+	.then(function(match) {
+	})
+	.catch(function(error) {
+	  console.log('There was an error retrieving match data', error);
+	});
+    }
   }
 }
--- wvw.js
+++ wvw.js
@@ -315,45 +315,5 @@ function updateStatus() {//{{{
   }
 }//}}}
 
-class MatchStatus {
-  constructor() {
-    this._		= {};
-    this._.lastUpdate 	= 0;
-    this._.tick		= 500;
-    this._.interval	= 5000;
-    this._.wid;
-    this._.matchActive	= false;
-    this._.baseURL	= 'https://api.guildwars2.com/v2/wvw/matches?world_id=';
-  }
-  start(wid) {
-    if (this._.wid != wid) {
-      this._.wid = wid;
-    }
-    this._.matchActive	= true;
-    this._.lastUpdate	= 0;
-  }
-  stop() {
-    this._.matchActive	= false;
-  }
-  get url {
-    if (this._.wid != undefined) {
-      return this._.baseURL + this._.wid;
-    } else {
-      return undefined;
-    }
-  }
-  update() {
-    if (this._.matchActive && (this.url != undefined)) {
-      fetch(this.url)
-	.then(status)
-	.then(function(response) { return response.json() })
-	.then(function(match) {
-	})
-	.catch(function(error) {
-	  console.log('There was an error retrieving match data', error);
-	});
-    }
-  }
-}
   updateStatus();
   ws.interval(window.setInterval(updateStatus,tick));
